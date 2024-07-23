5e72f86d1c23705c0efb328a4ea7412f805d4ff3
Hatai Pan <manbuzhe2008@gmail.com>
2024-07-23 18:52:01 +0800

    add Dockerfile && k8s deploy manifests

--- rollup.config.js
+++ rollup.config.js
@@ -131,7 +131,7 @@ function serve() {
       if (server) return;
       server = require("child_process").spawn(
         "npm",
-        ["run", "start", "--", "--dev"],
+        ["run", "start", "--", "--dev", "--host", "--port", "5173"],
         {
           stdio: ["ignore", "inherit", "inherit"],
           shell: true,
